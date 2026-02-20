51e0d6be6c655988878c1add8c1579d585e06b55
bsmin <bladiminga46@gmail.com>
2026-02-20 16:58:08 -0500

    Add Liquibase changelogs for tasks and users schemas and tables

--- msa-user-mgmt/build.gradle.kts
+++ msa-user-mgmt/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-liquibase")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenApiVersion")
     implementation("jakarta.validation:jakarta.validation-api:$jakartaValidationVersion")
     implementation("org.mapstruct:mapstruct:$mapstructVersion")
