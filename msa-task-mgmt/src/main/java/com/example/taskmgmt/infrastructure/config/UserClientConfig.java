6340bf65300a0c00828a91733a37e50904c018c6
bsmin <bladiminga46@gmail.com>
2026-02-22 01:03:36 -0500

    Refactor UserClientConfig to simplify UserManagementApi bean creation

--- msa-task-mgmt/src/main/java/com/example/taskmgmt/infrastructure/config/UserClientConfig.java
+++ msa-task-mgmt/src/main/java/com/example/taskmgmt/infrastructure/config/UserClientConfig.java
@@ -1,26 +1,16 @@
 package com.example.taskmgmt.infrastructure.config;
 
 import com.example.taskmgmt.infrastructure.adapter.out.client.user.api.UserManagementApi;
-import com.example.taskmgmt.infrastructure.adapter.out.client.user.invoker.ApiClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserClientConfig {
-
-    @Value("${services.user-mgmt.base-url}")
-    private String userMgmtBaseUrl;
-
     @Bean
-    public ApiClient userApiClient() {
-        ApiClient apiClient = new ApiClient();
-        apiClient.setBasePath(userMgmtBaseUrl);
-        return apiClient;
-    }
-
-    @Bean
-    public UserManagementApi userManagementApi(ApiClient userApiClient) {
-        return new UserManagementApi(userApiClient);
+    public UserManagementApi userManagementApi(@Value("${services.user-mgmt.base-url}") String userMgmtBaseUrl) {
+        var userManagementApi = new UserManagementApi();
+        userManagementApi.getApiClient().setBasePath(userMgmtBaseUrl);
+        return userManagementApi;
     }
 }
